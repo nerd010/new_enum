c6ca9ec412eeab8b3936dc3e847752dff35da5ef
Charles Wang <Charles@wangzengkangdemacbook-pro.local>
2016-03-13 10:28:00 +0800

    Initial Commit

---
+++ new_enum/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  new_enum
+//
+//  Created by Charles Wang on 16/3/13.
+//  Copyright © 2016年 CHW. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
